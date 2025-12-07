0c52942cbbd79dd4ab55e78ed3548924a33f1c3e
earthwittawat <earthwittawat2005@gmail.com>
2025-12-07 17:14:04 +0700

    feat(API): Order

---
+++ src/merchant/order/index.ts
@@ -0,0 +1,50 @@
+// src/routes/merchant.menu.ts
+import { Elysia } from 'elysia'
+import { PrismaClient } from '@prisma/client'
+import {
+  OrderStatusUpdateSchema
+
+} from '../../../types'
+
+const prisma = new PrismaClient()
+
+export const merchantOrder = new Elysia({ prefix: '/merchant' })
+
+  /* ===================== MENU ===================== */
+
+   .get('/:merchantId/order', async ({ params, set }) => {
+    const orders = await prisma.order.findMany({
+      where: { merchantId: params.merchantId },
+      include: {
+        items: {
+          include: { options: true },
+        },
+        customer: true,
+      },
+      orderBy: { createdAt: 'desc' },
+    })
+
+    if (!orders.length) {
+      set.status = 404
+      return { message: 'No orders found' }
+    }
+    return orders
+  }, { tags: ['Merchant'] })
+
+
+    .put('/:merchantId/order/:orderId', async ({ params, body, set }) => {
+      const order = await prisma.order.findFirst({
+        where: { id: params.orderId, merchantId: params.merchantId },
+      })
+      if (!order) {
+        set.status = 404
+        return { message: 'order not found' }
+      }
+  
+      const orderUpdated = await prisma.order.update({
+        where: { id: order.id },
+        data: {status: body.status}
+      })
+  
+      return orderUpdated
+    }, { body: OrderStatusUpdateSchema, tags: ['Merchant'] })
